a8990cef9fc05941492fb6d412863bdb1ba8157b
Mia Katz <mkatz21@choate.edu>
2018-09-14 11:08:04 -0400

    Fixed with 0.16

--- temp.py
+++ temp.py
@@ -2,7 +2,7 @@
 #This is the effective wind chill temperature calculator, argument 1 is the current temperature in fahrenheit, and the second is the velocity.
 
 import sys
-t = float(sys.argv[1])
-v = float(sys.argv[2])
-w =  35.74  +  0.6215* t  +  (0.4275*t  -  35.75)* v
-print("Effective Temperature with Wind Chill:",w)
+t = int(sys.argv[1])
+v = int(sys.argv[2])
+w = float(35.74  +  (0.6215* t)  +  ((0.4275*t*v) - (35.75) *v**0.16)	
+print("Temperature with Wind Chill:",w " Degrees F")
