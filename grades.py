7d6212dec4aed9982b7ed6f7263977b5a55bf266
Mia Katz <mkatz21@choate.edu>
2018-09-14 22:31:59 -0400

    Homework

---
+++ grades.py
@@ -0,0 +1,36 @@
+#Mia Katz 9.17.18
+#The program that tells you your letter grade based on the number you input
+#I didn't use any sources for this, though I tried to condense it as much as possible. The biggest challenge was that I made the mistake of omitting and and or, but it was helpful to include them.
+import sys
+
+grade = float(sys.argv[1])
+ten = float(grade%1)
+
+if grade > 5:
+	print("Enter a Value Between 0 and 5, please")
+if grade >= 4.5:
+    a = str("A")
+elif 3.5 <= grade < 4.5:
+    a = str("B")
+elif 2.5 <= grade < 3.5:
+    a = str("C")
+elif 1 <= grade:
+    a = str("D")
+else:
+    a = str("F")
+
+
+
+
+
+if (grade < 4 and (.85 <= ten and ten < .2)) or (4.7<=grade<4.85) or (1.5<=grade<2):
+    b = str(" ")
+elif (grade >= 4.85) or ((grade > 2) and (.2 <= ten < .5)) or (2 <= grade < 2.5):
+    b = str("+")
+elif (.5<= ten < .85) or (grade > 4 and .5 <= ten < .7) or (1<=grade<1.5):
+	b = str("-")
+else:
+	b = str(" ")
+
+if grade <= 5:
+	print("Your Grade is:",a,b)
---
+++ weather.py
@@ -0,0 +1,8 @@
+ans = input("How is the weather today? ")
+ans = str(ans)
+if ans == "rainy":
+    print("h o p e.  y o u.  b r o u g h t.   y o u r.   u m b r e l l a. ")
+elif ans == "nice":
+	print("ha. ha. ha. ha. ha. haaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahhahahahahaha aaaaaaaaaahhhhhhhhhahahahahhahahahaahhaha aaaaaaaaaaahhhhhhhh")
+else :
+    print("that's nice, dear")
