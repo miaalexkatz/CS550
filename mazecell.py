2ffa286809a6714658517b60bca97242317d6f3c
Mia Katz <mkatz21@choate.edu>
2018-11-04 18:01:28 -0500

    11.4.18

--- mazecell.py
+++ mazecell.py
@@ -1,4 +1,4 @@
-
+import random 
 class Cell:
 
 	def __init__(self):
@@ -7,7 +7,37 @@ class Cell:
 		self.downwall = True
 		self.rightwall = True
 		self.isVisited = False
+		self.frontier = False
 		self.adjacentchecks = 0
+		self.identity = "|_|"
+
+	def selected(self):
+		chance = random.randrange(4)
+		if chance < 1:
+			#go up
+
+	def finish(self):
+		if self.upwall == True:
+			if self.leftwall == True:
+				self.identity = "|  "
+			elif self.rightall == True:
+				self.identity = "  |"
+			elif self.downwall = True:
+				self.identity = "___"
+		elif self.rightwall == True:
+			if self.leftwall == True:
+				self.identity = "| |"
+			if self.downall == True:
+				self.identity = "__|"
+		elif self.leftwall == True:
+			if self.downwall == True:
+				self.identity = "|__"
+		else:
+			self.identity = "   "
+
+	def __str__(self):
+		return self.identity
+
 
 #to do: set the cell graphics (|_| and so on)
 
--- prim.py
+++ prim.py
@@ -1,18 +1,53 @@
 import sys
+import random
 from mazecell import Cell
 
 width = int(sys.argv[1])
 height = int(sys.argv[2])
 
-global t
-ex = [['X'] * width for x in range(height)+1]
+global ex
+ex = [['__|'] * (width+1) for x in range(height+1)]
+usedcells = []
+frontier = []
+for x in range(height+1):
+	ex[x][0] = "|"
 for x in range(width):
-
+	ex[0][x] = '____'
+#ex[0][width+1] = '_'
 for a in range(len(ex)):
 	print(*ex[a])
+selectedx = random.randrange(1, width)
+selectedy = random.randrange(1, height)
+while len(usedcells)<=width*height:
+	usedcells.append(ex[selectedx][selectedy])
+	if selectedx+1 != height+1 and selectedy+1 != height+1:
+		frontiercells.append(ex[selectedx+1][selectedy+1])
+	if selectedx+1 != height+1 and selectedy-1 != 0:
+		frontiercells.append(ex[selectedx+1][selectedy-1])
+	if selectedx-1 != 0 and selectedy +1 != height+1:
+		frontiercells.append(ex[selectedx-1][selectedy+1])
+	if selectedx-1 != 0 and selectedy != 0:
+		frontiercells.append(ex[selectedx-1][selectedy-1])
+	numero = random.randint(len(frontiercells))
+
+
+
+
 #ideas for before I forget:
 #the maze board is a list of Cells arranged in an array like Minesweeper
 #Each is composed of 4 walls
 #A wall value is set to False when the cell next to it randomly selects it. Then the cell will lose the wall adjacent to the cell that selected it
 #A cell can only lose two walls, and cannot visit a visited cell. The only exception is cells on the border!
-#Cells will apply a wall to the top, not the bottom. If a cell has upwall = True, then the above cell should have downwall == False. The bottom row of the list will be a border, which is why height has one added
+#Cells will apply a wall to the bottom, not the top. If a cell has downwall = True, then the above cell should have upwall == False. The top row of the list will be a border, which is why height has one added. This is because I can't find an up thing
+
+#randomly select a cell
+#append cell to list of cells
+#when the len of the used cells list is equivalent to the total area it all prints.
+
+
+#chance = random.randint(3)
+#if chance < 1:
+#	print("hi")
+		#go up
+		#ex[xsel][ysel] 
+	#elif 1< chance
