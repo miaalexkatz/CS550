a1edb3ff122b5d0b2c20ca32464dcac72b35cf92
Mia Katz <mkatz21@choate.edu>
2018-11-11 14:53:15 -0500

    working!

--- mazecell.py
+++ mazecell.py
@@ -1,65 +1,28 @@
 import random 
 class Cell:
 	def __init__(self):
-		self.upwall = True
-		self.leftwall = True
 		self.downwall = True
-		self.rightwall = True
 		self.identity = "|_|"
 
 	def finish(self):
 	#	if self.upwall == True:
 		if self.downwall == False:
 			self.identity = ".  "     
-		else:                               #UT LT DF R
-			self.identity = ".--"                                    #UT LT DT RT
-		# 		else:
-		# 			self.identity = "-+--+-"                                   #UT LT DT RF
-		# else:
-		# 	if self.downwall == False:
-		# 		if self.rightwall == True:
-		# 			self.identity = "-+  +-"                                    #UT LF DF RT 
-		# 		else:
-		# 			self.identity = "-+  +-"                                    #UT LF DF RF
-		# 	else:
-		# 		if self.rightwall == True:
-		# 			self.identity = "-+--+-"                                    #UT LF DT RT
-		# 		else:
-		# 			self.identity = ".--"                                    #UT LF DT RF
-
+		else:                               
+			self.identity = ".--"                                    
 	def __str__(self):
 		return self.identity
 # note: left wall takes precedent.
 class Walls:
 	def __init__(self):
-		self.upwall = True
 		self.leftwall = True
-		self.downwall = True
-		self.rightwall = True
 		self.identity = "|_|"
 	
 	def walls(self):
 		if self.leftwall == True:
-			if self.rightwall == True:
-				self.identity = "|  "                                    #UT LT DF RT
-			else:
-					self.identity = "|  "                                    #UT LT DF RF
+			self.identity = "|  "                                    
 		else:
-				self.identity = "   "                                    #UT LT DT RT
-  #                                #UT LT DT RF
-		# else:
-		# 	if self.downwall == False:
-		# 		if self.rightwall == True:
-		# 			self.identity = "    | "                                    #UT LF DF RT 
-		# 		else:
-		# 			self.identity = "      "                                    #UT LF DF RF
-		# 	else:
-		# 		if self.rightwall == True:
-		# 			self.identity = "    | "                                    #UT LF DT RT
-		# 		else:
-		# 			self.identity = "      "                                    #UT LF DT RF
-
-
+				self.identity = "   "                                   
 	def __str__(self):
 		return self.identity
 
--- prim.py
+++ prim.py
@@ -7,112 +7,103 @@ import sys
 import random
 from mazecell import Cell, Walls
 
-width = int(sys.argv[1])
-height = int(sys.argv[2])
+columns = int(sys.argv[1])
+rows = int(sys.argv[2])
 
 global ex
-ex = [[".X."] * (width) for x in range(height*2+1)] #this sets up the board in general
-#test = [["hi"] * (width) for x in range(height)]
-cells = [[Cell() for i in range(width)]for j in range(height)] #the previous line may be unnecessary
-walls = [[Walls() for i in range(width)] for j in range(height)]
+ex = [[".X."] * (columns) for x in range(rows*2+1)] #this sets up the board in general
+cells = [[Cell() for i in range(columns)]for j in range(rows)] #stores the values for the bottom cell walls
+walls = [[Walls() for i in range(columns)] for j in range(rows)] #stores the vertical cell walls
 visitedcells = [] #the list of cells that have been selected already
 frontiercells = [] #the list of cells on the frontier which can be selected next
-selectedx = 0 #sets the start point in the corner
-selectedy = 0
-print(selectedx,selectedy)
+realnumber = 1 #sets the start point in the corner. The values will be subtracted from eventually to be 0,0. The complex negates 0 as a real number during wall selections.
+imagnumber = 1
 
-while len(visitedcells) <= ((height+1)*(width+1)):
+
+visitedcells.append(complex(realnumber, imagnumber))
+print(realnumber,imagnumber)
+frontiercells.append(complex(realnumber+1, imagnumber))
+frontiercells.append(complex(realnumber, imagnumber+1))
+
+while len(visitedcells) <= ((rows+1)*(columns+1)):
+	if len(frontiercells) > 0:
+		number = random.randint(0, (len(frontiercells)-1)) #this picks the next values from the complex number stored in the frontier 
+		print(realnumber,imagnumber, frontiercells)
+		realnumber = int(frontiercells[number].real) #these two functions convert the stored complex into values
+		imagnumber = int(frontiercells[number].imag)
+		frontiercells.remove(complex(realnumber, imagnumber))
+		visitedcells.append(complex(realnumber, imagnumber))	
+	else: 
+		break
 	chance0, chance1, chance2, chance3 = False, False, False, False
 	itWorked = False
-	visitedcells.append(complex(selectedx, selectedy))	
-	if selectedx+1 < height and complex(selectedx+1, selectedy) not in frontiercells and complex(selectedx+1, selectedy) not in visitedcells: #the if statements check that the value added to frontier is actually in the usable board
-		frontiercells.append(complex(selectedx+1, selectedy)) #this adds to the frontier list
+	if realnumber < rows and realnumber != 0 and complex(realnumber+1, imagnumber) not in frontiercells and complex(realnumber+1, imagnumber) not in visitedcells: #the if statements check that the value added to frontier is actually in the usable board
+		frontiercells.append(complex(realnumber+1, imagnumber)) #this adds to the frontier list
 
-	if selectedy != 0 and complex(selectedx, selectedy-1) not in frontiercells and complex(selectedx, selectedy-1) not in visitedcells:
-		frontiercells.append(complex(selectedx,selectedy-1))
-	elif selectedy == 0:
-		frontiercells.append(complex(selectedx, selectedy+1))
+	if imagnumber != 1 and complex(realnumber, imagnumber-1) not in frontiercells and complex(realnumber, imagnumber-1) not in visitedcells:
+		frontiercells.append(complex(realnumber,imagnumber-1))
+#	elif imagnumber == 1:
+#		frontiercells.append(complex(realnumber, imagnumber+1))
 
-	if selectedx != 0 and complex(selectedx-1, selectedy) not in frontiercells and complex(selectedx-1, selectedy) not in visitedcells:
-		frontiercells.append(complex(selectedx-1,selectedy))
-	elif selectedx == 0:
-		frontiercells.append(complex(selectedx+1, selectedy))
+	if realnumber != 1 and complex(realnumber-1, imagnumber) not in frontiercells and complex(realnumber-1, imagnumber) not in visitedcells:
+		frontiercells.append(complex(realnumber-1,imagnumber))
+#	elif realnumber == 1:
+#		frontiercells.append(complex(realnumber+1, imagnumber))
 
-	if selectedy+1 < width and complex(selectedx, selectedy+1) not in frontiercells and complex(selectedx, selectedy+1) not in visitedcells:
-		frontiercells.append(complex(selectedx,selectedy+1))
+	if imagnumber<columns and imagnumber!=0 and complex(realnumber,imagnumber+1) not in frontiercells and complex(realnumber,imagnumber+1) not in visitedcells:
+		frontiercells.append(complex(realnumber,imagnumber+1))
 	while itWorked != True:
-		if len(visitedcells) != (height+1)*(width+1):
-			#if selectedx != 0 and selectedy != 0:
-			#if selectedx != 0 and selectedy != 0 and selectedy != height-1 and selectedx != width-1:
+		if len(visitedcells) != (rows+1)*(columns+1):
+			#if realnumber != 0 and imagnumber != 0:
+			#if realnumber != 0 and imagnumber != 0 and imagnumber != rows-1 and realnumber != columns-1:
 			chance = random.randint(0,3) #whether it will go up, down, left, right
 		if (chance0 == True and chance1 == True and chance2 == True and chance3 == True):
 			itWorked = True
 		elif chance == 0:
-			if complex(selectedx,selectedy+1) in visitedcells: #go down
-				cells[selectedx][selectedy+1].upwall = False
-				cells[selectedx][selectedy].downwall = False
+			if complex(realnumber+1,imagnumber) in visitedcells: #go down
+				cells[realnumber-1][imagnumber-1].downwall = False
 				itWorked = True
 			chance0 = True
 		elif chance == 2:
-			if complex(selectedx-1,selectedy) in visitedcells: #right
-				walls[selectedx-1][selectedy].rightwall = False
-				walls[selectedx][selectedy].leftwall = False
+			if complex(realnumber,imagnumber-1) in visitedcells: #right
+				walls[realnumber-1][imagnumber-1].leftwall = False
 				itWorked = True
 			chance2 = True
 		elif chance == 1:
-			if complex(selectedx,selectedy-1) in visitedcells: #go down
-				cells[selectedx][selectedy-1].downwall = False
-				cells[selectedx][selectedy].upwall = False
+			if complex(realnumber-1,imagnumber) in visitedcells: #go up
+				cells[realnumber-2][imagnumber-1].downwall = False
 				itWorked = True
 			chance1 = True
          #go down
 		elif chance == 3: 
-			if complex(selectedx+1,selectedy) in visitedcells: #go left
-				walls[selectedx+1][selectedy].leftwall = False
-				walls[selectedx][selectedy].rightwall = False
+			if complex(realnumber,imagnumber+1) in visitedcells: #go left
+				walls[realnumber-1][imagnumber].leftwall = False
 				itWorked = True
 			chance3 = True
-#		print(chance0, chance1, chance2, chance3)
-#	for g in range(height):
-#		if selectedx == 0:
-#			walls[0][g].leftwall = True
-	if len(frontiercells) > 0:
-		number = random.randint(0, (len(frontiercells)-1)) #this picks the next values from the complex number stored in the frontier 
-		print(selectedx,selectedy, frontiercells)
-		selectedx = int(frontiercells[number].real) #these two functions convert the stored complex into values
-		selectedy = int(frontiercells[number].imag)
-		frontiercells.remove(complex(selectedx, selectedy))
-	else: 
-		break
-for c in range(int(height/2)):
+for c in range(int(rows/2)):
 	walls[c][0].leftwall = True
-for d in range(width):
-	cells[height-1][d].downwall = True
-#for c in range((height*2)-2):
-#	walls[c][width].rightwall = True
-#for be in range(width):
-#	walls[be][height].downwall = True
-for x in range(height):
-	for y in range(width):
+for d in range(columns):
+	cells[rows-1][d].downwall = True
+for x in range(rows):
+	for y in range(columns):
 		cells[x][y].finish()
 		walls[x][y].walls()
 
-for x in range(width):
+for x in range(columns):
 	ex[0][x] = ".--"
-	for y in range(height):
+	for y in range(rows):
 		ex[(y*2)+1][x] = walls[y][x]
 		ex[(y*2)+2][x] = cells[y][x]
-for y in range(height):
-	ex[(y*2)+1].insert(width, "|")
+for y in range(rows):
+	ex[(y*2)+1].insert(columns, "|")
 	ex[(y*2)+1][0] = "|  "
-for i in range(height+1):
-	ex[(i*2)].insert(width, ".")
-#for a in range(width):
+for i in range(rows+1):
+	ex[(i*2)].insert(columns, ".")
+#for a in range(columns):
 ex[0][0]="   "
 ex[1][0]="   "
-ex[height*2][width]=".  "
-ex[height*2-1][width]="   "
-walls[0][0].leftwall = False
+ex[rows*2][columns]=".  "
+ex[rows*2-1][columns]="   "
 for a in range(len(ex)):
 	print(*ex[a])
 
@@ -126,7 +117,7 @@ print(visitedcells)
 #Each is composed of 4 walls
 #A wall value is set to False when the cell next to it randomly selects it. Then the cell will lose the wall adjacent to the cell that selected it
 #A cell can only lose two walls, and cannot visit a visited cell. The only exception is cells on the border!
-#Cells will apply a wall to the bottom, not the top. If a cell has downwall = True, then the above cell should have upwall == False. The top row of the list will be a border, which is why height has one added. This is because I can't find an up thing
+#Cells will apply a wall to the bottom, not the top. If a cell has downwall = True, then the above cell should have upwall == False. The top row of the list will be a border, which is why rows has one added. This is because I can't find an up thing
 
 #randomly select a cell
 #append cell to list of cells
