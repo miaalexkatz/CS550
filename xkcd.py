8941a9a8160ff5253a09dc4837dc6d78e80948b8
Mia Katz <mkatz21@choate.edu>
2019-01-14 15:20:14 -0500

    ok

--- xkcd.py
+++ xkcd.py
@@ -4,10 +4,24 @@
 #50-65% water
 #one campfire takes about 3 gallons to extinguish
 #weight of a person in kg will account of L of water
+import random
 
 
 def personwater():
-	weight = random.randrange(45, 100):
-	waterper = random.randrange(5500, 6500)/10000
-	waterkg = weight*waterper
+	firesputout = 0
+	weight = random.randrange(57, 90)
+	waterper = random.randrange(5000, 6500)/10000
+	waterl = weight*waterper
+	firesize = random.randrange(330, 470)
+	lneeded = .04*firesize
+	while waterl - lneeded >=0:
+		waterl -= lneeded
+		firesputout += 1
+		firesize = random.randrange(330, 470)
+		lneeded = .04*firesize
+	firesputout += lneeded/waterl
+	extra = firesputout%.001
+	firesputout = firesputout-extra
+	return firesputout
+
 
