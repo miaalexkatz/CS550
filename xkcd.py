fd6faa4468591500c722322784c01aa8a1533007
Mia Katz <mkatz21@choate.edu>
2019-01-14 14:08:11 -0500

    project

---
+++ xkcd.py
@@ -0,0 +1,13 @@
+#Ideas for ridiculous question thing
+#if you took the L of water in an adult human body, how many camp fires could one person put out?
+
+#50-65% water
+#one campfire takes about 3 gallons to extinguish
+#weight of a person in kg will account of L of water
+
+
+def personwater():
+	weight = random.randrange(45, 100):
+	waterper = random.randrange(5500, 6500)/10000
+	waterkg = weight*waterper
+
