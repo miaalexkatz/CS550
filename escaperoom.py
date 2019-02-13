ab27733aa694a3c023befdacb81b85298369038a
Mia Katz <mkatz21@choate.edu>
2019-02-12 20:44:14 -0500

    images

--- escaperoom.py
+++ escaperoom.py
@@ -1,4 +1,13 @@
 #Mia Katz and Steph Hotz idk the last name sorry
 
 import pygame
+screen = pygame.display.set_mode((1000, 750)) 
+
+
+done = False
+while not done:
+	for event in pygame.event.get(): 
+		if event.type == pygame.QUIT: 
+			done = True
+
 
